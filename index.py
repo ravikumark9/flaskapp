dae3a1b3076c330c17aa283c101c1c75bc180273
ravikumark9 <ravikumark9@gmail.com>
2020-02-27 11:36:20 +0530

    Update index.py

--- index.py
+++ index.py
@@ -4,4 +4,4 @@ app = Flask(__name__)
 def hello():
     return "Hello World!"
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int("9001"), debug=True)
+    app.run(host="0.0.0.0", port=int("9000"), debug=True)
